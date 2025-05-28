dbff8985e8c256df95e4a2e630e84898d896f83d
Protein-informatics <rukmankesh@iitbhilai.ac.in>
2025-05-28 18:31:28 +0530

    Add files via upload

---
+++ src/GraphSAGE.py
@@ -0,0 +1,196 @@
+import argparse
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, SAGEConv
+from eval import Evaluator
+from logger import Logger
+from utils import train, test, get_dataset, LinkPredictor
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
+        super(GCN, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(GCNConv(in_channels, hidden_channels, cached=True))
+        for _ in range(num_layers - 2):
+            self.convs.append(GCNConv(hidden_channels, hidden_channels, cached=True))
+        self.convs.append(GCNConv(hidden_channels, out_channels, cached=True))
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, adj_t):
+        for conv in self.convs[:-1]:
+            x = conv(x, adj_t)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t)
+        return x
+
+
+class SAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
+        super(SAGE, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden_channels, hidden_channels))
+        self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, adj_t):
+        for conv in self.convs[:-1]:
+            x = conv(x, adj_t)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t)
+        return x
+
+
+def main():
+    parser = argparse.ArgumentParser(description="OGBL-DKP (GNN)")
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--log_steps", type=int, default=5)
+    parser.add_argument("--use_sage", action="store_true")
+    parser.add_argument("--num_layers", type=int, default=2)
+    parser.add_argument("--hidden_channels", type=int, default=256)
+    parser.add_argument("--dropout", type=float, default=0.5)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--epochs", type=int, default=150)
+    parser.add_argument("--eval_steps", type=int, default=5)
+    parser.add_argument("--runs", type=int, default=5)
+    parser.add_argument("--model_dir", type=str, default="input/ogbl_dkp/processed")
+    args = parser.parse_args()
+    print(args)
+
+    device = f"cuda:{args.device}" if torch.cuda.is_available() else "cpu"
+    device = torch.device(device)
+
+    dataset = get_dataset()
+    data = dataset[0]
+    adj_t = data["edge_index"].to(device)
+    split_edge = dataset.get_edge_split()
+
+    # We randomly pick some training samples that we want to evaluate on:
+    torch.manual_seed(12345)
+
+    if args.use_sage:
+        model = SAGE(
+            args.hidden_channels,
+            args.hidden_channels,
+            args.hidden_channels,
+            args.num_layers,
+            args.dropout,
+        ).to(device)
+    else:
+        model = GCN(
+            args.hidden_channels,
+            args.hidden_channels,
+            args.hidden_channels,
+            args.num_layers,
+            args.dropout,
+        ).to(device)
+
+    emb = torch.nn.Embedding(data.num_nodes, args.hidden_channels).to(device)
+    predictor = LinkPredictor(
+        args.hidden_channels, args.hidden_channels, 2, args.num_layers, args.dropout
+    ).to(device)
+
+    evaluator = Evaluator("classification")
+    loggers = {
+        "Precision": Logger(args.runs, args),
+        "Recall": Logger(args.runs, args),
+        "F1": Logger(args.runs, args),
+        "Accuracy": Logger(args.runs, args),
+    }
+
+    for run in range(args.runs):
+        best_f1 = 0
+        torch.nn.init.xavier_uniform_(emb.weight)
+        model.reset_parameters()
+        predictor.reset_parameters()
+        optimizer = torch.optim.Adam(
+            list(model.parameters())
+            + list(emb.parameters())
+            + list(predictor.parameters()),
+            lr=args.lr,
+        )
+
+        for epoch in range(1, 1 + args.epochs):
+            loss = train(
+                model,
+                predictor,
+                emb.weight,
+                adj_t,
+                split_edge,
+                optimizer,
+                args.batch_size,
+            )
+
+            if epoch % args.eval_steps == 0:
+                results = test(
+                    model,
+                    predictor,
+                    emb.weight,
+                    adj_t,
+                    split_edge,
+                    evaluator,
+                    args.batch_size,
+                )
+                for key, result in results.items():
+                    loggers[key].add_result(run, result)
+
+                print(loggers["F1"].results[run])
+                if loggers["F1"].results[run][0][0] > best_f1:
+                    best_f1 = loggers["F1"].results[run][0][0]
+                    if args.use_sage:
+                        torch.save(
+                            model.state_dict(), f"{args.model_dir}/sage_{run}.pth"
+                        )
+                        torch.save(
+                            predictor.state_dict(),
+                            f"{args.model_dir}/sage_predictor_{run}.pth",
+                        )
+                    else:
+                        torch.save(
+                            model.state_dict(), f"{args.model_dir}/gnn_{run}.pth"
+                        )
+                        torch.save(
+                            predictor.state_dict(),
+                            f"{args.model_dir}/gnn_predictor_{run}.pth",
+                        )
+
+                if epoch % args.log_steps == 0:
+                    for key, result in results.items():
+                        valid_hits, test_hits = result
+                        print(key)
+                        print(
+                            f"Run: {run + 1:02d}, "
+                            f"Epoch: {epoch:02d}, "
+                            f"Loss: {loss:.4f}, "
+                            f"Valid: {100 * valid_hits:.2f}%, "
+                            f"Test: {100 * test_hits:.2f}%"
+                        )
+                    print("---")
+
+        for key in loggers.keys():
+            print(key)
+            loggers[key].print_statistics(run)
+
+    for key in loggers.keys():
+        print(key)
+        loggers[key].print_statistics()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/UniGENDDI.py
@@ -0,0 +1,270 @@
+# DANN-DDI
+import os
+import json
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import numpy as np
+import pandas as pd
+import csv
+import pickle
+
+import graph
+import keras
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import precision_score
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, Activation, BatchNormalization
+from keras.utils import to_categorical
+
+event_num = 2
+droprate = 0.4
+vector_size = 128
+SPLIT = "../split/demo"
+
+
+def DNN():
+    train_input1 = Input(shape=(456,), name="Inputlayer1")
+    train_input2 = Input(shape=(456,), name="Inputlayer2")
+    # train_input1 = Input(shape=(vector_size * 5 + 456,), name="Inputlayer1")
+    # train_input2 = Input(shape=(vector_size * 5 + 456,), name="Inputlayer2")
+
+    # Attention Neural Network
+    train_input = keras.layers.Concatenate()([train_input1, train_input2])
+
+    attention_probs = Dense(
+        456,
+        activation="relu",
+        use_bias=True,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+        name="attention1",
+    )(train_input)
+
+    att = Dense(
+        456,
+        activation="softmax",
+        kernel_initializer="random_uniform",
+        name="attention",
+    )(attention_probs)
+
+    vec = keras.layers.Multiply()([train_input1, train_input2])
+    attention_mul = keras.layers.Multiply()([vec, att])
+
+    # Deep neural network classifier
+    train_in = Dense(4096, activation="relu", name="FullyConnectLayer1")(attention_mul)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(2048, activation="relu", name="FullyConnectLayer2")(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(1024, activation="relu", name="FullyConnectLayer3")(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(512, activation="relu", name="FullyConnectLayer4")(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(256, activation="relu", name="FullyConnectLayer5")(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(128, activation="relu", name="FullyConnectLayer6")(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(64, activation="relu", name="FullyConnectLayer7")(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+
+    train_in = Dense(event_num, name="SoftmaxLayer")(train_in)
+
+    out = Activation("softmax", name="OutputLayer")(train_in)
+
+    model = Model(inputs=[train_input1, train_input2], outputs=out)
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    return model
+
+
+def calculate_metric_score(real_labels, predict_score):
+    # Evaluate the prediction performance
+    precision, recall, pr_thresholds = precision_recall_curve(
+        real_labels, predict_score
+    )
+    aupr_score = auc(recall, precision)
+    all_F_measure = np.zeros(len(pr_thresholds))
+    for k in range(0, len(pr_thresholds)):
+        if (precision[k] + recall[k]) > 0:
+            all_F_measure[k] = 2 * precision[k] * recall[k] / (precision[k] + recall[k])
+        else:
+            all_F_measure[k] = 0
+    print("all_F_measure: ")
+    print(all_F_measure)
+    max_index = all_F_measure.argmax()
+    threshold = pr_thresholds[max_index]
+    fpr, tpr, auc_thresholds = roc_curve(real_labels, predict_score)
+    auc_score = auc(fpr, tpr)
+
+    f = f1_score(real_labels, predict_score)
+    print("F_measure:" + str(all_F_measure[max_index]))
+    print("f-score:" + str(f))
+    accuracy = accuracy_score(real_labels, predict_score)
+    precision = precision_score(real_labels, predict_score)
+    recall = recall_score(real_labels, predict_score)
+    print("results for feature:" + "weighted_scoring")
+    print(
+        "*** AUC score:%.3f, AUPR score:%.3f, precision score:%.3f, recall score:%.3f, f score:%.3f,accuracy:%.3f ***"
+        % (auc_score, aupr_score, precision, recall, f, accuracy)
+    )
+    results = {
+        "auc_score": auc_score,
+        "aupr_score": aupr_score,
+        "precision": precision,
+        "recall": recall,
+        "f": f,
+        "accuracy": accuracy,
+    }
+
+    return results
+
+
+def cross_validation():
+    #  Build the drug-drug interaction network
+    g = graph.Graph()
+    g.read_edgelist("input/ogbl_dkp/mapping/drug_drug.txt")
+    print(g.G.number_of_edges())
+
+    # Remove the test_links in the network
+    edge_list = {}
+    for split in ["train", "val", "test"]:
+        edge_list[split] = pickle.load(open(f"{SPLIT}/{split}.pkl", "rb"))
+
+    for split in ["val", "test"]:
+        edges = edge_list[split]["edge"]
+        for i in range(edges.shape[0]):
+            n1, n2 = edges[0, i], edges[1, i]
+            if g.G.has_edge(str(n1), str(n2)):
+                g.G.remove_edge(str(n1), str(n2))
+
+    # Obtain representation vectors by SDNE
+    print("Test Begin")
+
+    model_n2v = loadmodel("input/ogbl_dkp/processed/n2v.csv")
+    model_sage = loadmodel("input/ogbl_dkp/processed/sage_emb.csv")
+
+    # Concatenate of representation vectors generated by five drug feature networks
+    E = np.zeros((841, 456), float)
+    for i in range(841):
+        E[i][0:200] = model_n2v[i]
+        E[i][200:456] = model_sage[i]
+
+    # Training set
+    X_train1 = []
+    X_train2 = []
+    Y_train = []
+
+    train_pos = edge_list["train"]["edge"]
+    label_pos = [1] * train_pos.shape[1]
+    pos_x1 = E[train_pos[0, :]]
+    pos_x2 = E[train_pos[1, :]]
+    Y_train.extend(label_pos)
+
+    train_neg = edge_list["train"]["edge_neg"]
+    label_neg = [0] * train_neg.shape[1]
+    neg_x1 = E[train_neg[0, :]]
+    neg_x2 = E[train_neg[1, :]]
+    Y_train.extend(label_neg)
+
+    X_train1 = np.concatenate([pos_x1, neg_x1], axis=0)
+    X_train2 = np.concatenate([pos_x2, neg_x2], axis=0)
+    Y_train = np.array(Y_train)
+    Y_train = to_categorical(Y_train, 2)
+    del train_pos, train_neg, label_pos, label_neg
+
+    print(X_train1.shape, X_train2.shape, Y_train.shape)
+    dnn = DNN()
+    dnn.fit(
+        [X_train1, X_train2],
+        Y_train,
+        batch_size=128,
+        epochs=150,
+        shuffle=True,
+        verbose=1,
+    )
+
+    # Test set
+    X_test1 = []
+    X_test2 = []
+    Y_test = []
+
+    test_pos = edge_list["test"]["edge"]
+    label_pos = [1] * test_pos.shape[1]
+    pos_x1 = E[test_pos[0, :]]
+    pos_x2 = E[test_pos[1, :]]
+    Y_test.extend(label_pos)
+
+    test_neg = edge_list["test"]["edge_neg"]
+    label_neg = [0] * test_neg.shape[1]
+    neg_x1 = E[test_neg[0, :]]
+    neg_x2 = E[test_neg[1, :]]
+    Y_test.extend(label_neg)
+
+    X_test1 = np.concatenate([pos_x1, neg_x1], axis=0)
+    X_test2 = np.concatenate([pos_x2, neg_x2], axis=0)
+    del test_pos, test_neg, label_pos, label_neg
+
+    y_pred_label = dnn.predict([X_test1, X_test2])
+    y_pred_label = np.argmax(y_pred_label, axis=1)
+    y_pred_label = np.array(y_pred_label).tolist()
+    print("Final Predicted Labels:", y_pred_label)
+
+    return calculate_metric_score(Y_test, y_pred_label)
+
+
+def loadmodel(path):
+    # Load the files of representation vectors generated before
+    Emb = pd.read_csv(path, header=None)
+    features = list(Emb.columns)
+    Emb = np.array(Emb[features])
+    return Emb
+
+
+def main(runs):
+    log = open("demo_result.txt", "w")
+    results = []
+    for run in range(runs):
+        print("Executing Run", run)
+        result = cross_validation()
+        log.write(json.dumps(result) + "\n")
+        results.append(result)
+
+    # find the mean and std
+    auc_score = np.array([result["auc_score"] for result in results])
+    aupr_score = np.array([result["aupr_score"] for result in results])
+    precision = np.array([result["precision"] for result in results])
+    recall = np.array([result["recall"] for result in results])
+    f = np.array([result["f"] for result in results])
+    accuracy = np.array([result["accuracy"] for result in results])
+    final_results = {
+        "auc_score": str(np.mean(auc_score)) + "+/-" + str(np.std(auc_score)),
+        "aupr_score": str(np.mean(aupr_score)) + "+/-" + str(np.std(aupr_score)),
+        "precision": str(np.mean(precision)) + "+/-" + str(np.std(precision)),
+        "recall": str(np.mean(recall)) + "+/-" + str(np.std(recall)),
+        "f": str(np.mean(f)) + "+/-" + str(np.std(f)),
+        "accuracy": str(np.mean(accuracy)) + "+/-" + str(np.std(accuracy)),
+    }
+    log.write(json.dumps(final_results) + "\n")
+    log.close()
+
+
+if __name__ == "__main__":
+    main(5)
